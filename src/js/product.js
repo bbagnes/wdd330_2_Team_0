f803d01edd35c304f72e0a8086e683f17f6f5814
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-09-04 19:46:17 +0100

    update quotes 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
@@ -6,7 +6,7 @@ const dataSource = new ProductData('tents');
 function addProductToCart(product) {
   const cart = getLocalStorage('so-cart'); // always an array per your utils
   cart.push(product);
-  setLocalStorage("so-cart", cart);
+  setLocalStorage('so-cart', cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
