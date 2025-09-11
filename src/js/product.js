62dd842080130e353e6e03197da582fe4e3aca4e
bbagnes <bbagnes@byupathway.edu>
2025-09-11 00:52:11 -0600

    Found errors in code running lint. Corrected duplication call of the const 'product' and unused definition of dataDetails. Hid testing code within comments. Variable dataDetails renamed as productDetails for clarity. Removed duplicate function to add eventlistener to Add to Cart button.

--- src/js/product.js
+++ src/js/product.js
@@ -5,13 +5,12 @@ import ProductDetails from './ProductDetails.mjs';
 const dataSource = new ProductData('tents');
 
 const productId = getParam('product');
-const dataDetails = new ProductDetails(productId, dataSource);
+const productDetails = new ProductDetails(productId, dataSource);
 
-console.log(dataSource);
-const product = new ProductDetails(productId, dataSource);
-product.init();
+// console.log(dataSource);
+productDetails.init();
 
-console.log(dataSource.findProductById(productId));
+// console.log(dataSource.findProductById(productId));
 
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -20,11 +19,4 @@ async function addToCartHandler(e) {
 
   const product = await dataSource.findProductById(id);
   if (!product) return;
-
-  product.addProductToCart(product);
 }
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
