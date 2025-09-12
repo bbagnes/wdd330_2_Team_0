7bbd8a5df458ca5abf802ede97cb46b899d54ae3
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-09-12 17:36:14 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
 import ProductData from './ProductData.mjs';
-import { updateCartBadge } from './product.js';;
-
+import { updateCartBadge } from './product.js';
 
 const dataSource = new ProductData('tents');
-
-// (optional test)
-dataSource.getData().then(data => console.log('tents:', data));
 
 document.addEventListener('DOMContentLoaded', () => {
     updateCartBadge();
